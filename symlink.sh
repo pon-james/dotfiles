1c92a6fcd602eb625be600687cd8cabba98f4a98
bijenkorf-james-wakefield <james.wakefield@debijenkorf.nl>
2020-07-22 14:10:03 +0200

    feat(Symlinks): Add symlinks for vs code settings
    BREAKING CHANGE: n
    
    n

---
+++ symlink.sh
@@ -0,0 +1,26 @@
+
+#!/bin/bash
+
+# CONFIGURE GIT
+#ln -sf $(pwd)/.gitconfig ~/.gitconfig
+#ln -sf $(pwd)/.gitignore_global ~/.gitignore_global
+
+# VS Code
+echo "symlink VS code settings"
+ln -sf $(pwd)/vs-code-settings.json ~/Library/Application\ Support/Code/User/settings.json
+
+# SETUP ALIASES ETC
+#echo "symlink aliases and functions"
+#ln -sf $(pwd)/env.sh ~/env.sh
+
+#echo "symlink zshrc config"
+#ln -sf $(pwd)/.zshrc ~/.zshrc
+
+#echo "symlink hyper config"
+#ln -sf $(pwd)/.hyper.js ~/.hyper.js
+
+#echo "symlink gemrc"
+#ln -sf $(pwd)/.gemrc ~/.gemrc
+
+#echo "symlink .npmrc"
+#ls -sf $(pwd)/.npmrc ~/.npmrc
