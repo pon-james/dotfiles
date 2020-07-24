8908e912331f4300135f4a7bc2b06b6ac556a47f
bijenkorf-james-wakefield <james.wakefield@debijenkorf.nl>
2020-07-24 10:14:02 +0200

    feat(macos): add trackpad configuration

--- macos.sh
+++ macos.sh
@@ -16,3 +16,13 @@ sudo dscl . create /Users/$(whoami) Picture "$(pwd)/user-picture.png"
 defaults write com.apple.menuextra.battery ShowPercent YES
 
 
+############
+# Trackpad #
+############
+
+# Enable tap to click
+defaults write -g com.apple.mouse.tapBehavior -int 1
+
+# Enable three finger drag
+defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -int 1
+
