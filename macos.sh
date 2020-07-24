f9beebb87d5b0bf7ae5d8e0dae4b6d25e30dd8ff
bijenkorf-james-wakefield <james.wakefield@debijenkorf.nl>
2020-07-24 10:13:37 +0200

    feat(macos): add battery presentation settings

--- macos.sh
+++ macos.sh
@@ -3,8 +3,16 @@
 # Prompt for sudo access upfront
 sudo -v
 
+#################
+# User settings #
+#################
+
 ## Set login picture
 dscl . delete /Users/$(whoami) jpegphoto
 dscl . delete /Users/$(whoami) Picture
 sudo dscl . create /Users/$(whoami) Picture "$(pwd)/user-picture.png"
+
+# Menu bar: show battery percentage
+defaults write com.apple.menuextra.battery ShowPercent YES
+
 
