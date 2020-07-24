1c9a9e56e318e98454df3002a5f40951f2033384
bijenkorf-james-wakefield <james.wakefield@debijenkorf.nl>
2020-07-24 10:12:42 +0200

    chore(symlink): tidy up symlink.sh removing unused lines

--- symlink.sh
+++ symlink.sh
@@ -1,29 +1,14 @@
 
 #!/bin/bash
 
-# CONFIGURE GIT
-#ln -sf $(pwd)/.gitconfig ~/.gitconfig
-#ln -sf $(pwd)/.gitignore_global ~/.gitignore_global
-
 # VS Code
 echo "symlink VS code settings"
 ln -sf $(pwd)/vs-code-settings.json ~/Library/Application\ Support/Code/User/settings.json
 
-# SETUP ALIASES ETC
-#echo "symlink aliases and functions"
-#ln -sf $(pwd)/env.sh ~/env.sh
-
+# Zsh
 echo "symlink zshrc config"
 ln -sf $(pwd)/.zshrc ~/.zshrc
 
+# Commitizen
 echo "symlink czrc config"
 ln -sf $(pwd)/.czrc ~/.czrc
-
-#echo "symlink hyper config"
-#ln -sf $(pwd)/.hyper.js ~/.hyper.js
-
-#echo "symlink gemrc"
-#ln -sf $(pwd)/.gemrc ~/.gemrc
-
-#echo "symlink .npmrc"
-#ls -sf $(pwd)/.npmrc ~/.npmrc
